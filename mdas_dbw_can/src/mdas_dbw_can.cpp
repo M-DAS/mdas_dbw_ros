5c99013cdcf425d8223a9339c53cdae6fa9e62ae
Brandon Smyth <bjsmyth@umich.edu>
2019-05-14 13:14:53 -0400

    Message set and node start

---
+++ mdas_dbw_can/src/mdas_dbw_can.cpp
@@ -0,0 +1,75 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "can_msgs/Frame.h"
+#include "mdas_dbw_msgs/SteeringCmd.h"
+#include "mdas_dbw_msgs/BrakeReport.h"
+
+#include <sstream>
+#include <cstdint>
+
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "mdas_dbw");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  uint32_t throttle_id, steer_id, brake_id;
+  n.param<std::uint32_t>("throttle_can_id", throttle_id, 0x1ADB0000)
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+  ros::Rate loop_rate(10);
+
+  /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+  int count = 0;
+  while (ros::ok())
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+
+    can_msgs::Frame canMsg;
+
+    canMsg.id = 0x123;
+
+    ROS_INFO("%s", msg.data.c_str());
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
