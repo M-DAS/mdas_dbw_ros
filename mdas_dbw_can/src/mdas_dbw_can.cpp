d6ad8da8e8c0fd6c728314ef9b0a8879d6052e2c
Brandon Smyth <bjsmyth@umich.edu>
2019-05-14 17:33:53 -0400

    Working starter node

--- mdas_dbw_can/src/mdas_dbw_can.cpp
+++ mdas_dbw_can/src/mdas_dbw_can.cpp
@@ -1,75 +1,71 @@
 #include "ros/ros.h"
+
 #include "std_msgs/String.h"
 #include "can_msgs/Frame.h"
+
 #include "mdas_dbw_msgs/SteeringCmd.h"
+#include "mdas_dbw_msgs/SteeringReport.h"
+#include "mdas_dbw_msgs/BrakeCmd.h"
 #include "mdas_dbw_msgs/BrakeReport.h"
+#include "mdas_dbw_msgs/ThrottleReport.h"
+#include "mdas_dbw_msgs/ThrottleCmd.h"
 
 #include <sstream>
 #include <cstdint>
 
+int throttle_id, steer_id, brake_id, feedback_id;
+ros::Publisher throttlePub, steeringPub, brakePub;
+
+
+void receivedMessagesCallback(const can_msgs::Frame::ConstPtr& canFrame) {
+  if(canFrame->id != feedback_id) {
+    return;
+  }
+  std::uint8_t srcId = canFrame->data[0];
+
+  switch(srcId) {
+    case 1:
+      mdas_dbw_msgs::ThrottleReport msg;
+      msg.enabled = 0;
+      msg.pedal_input = canFrame->data[2];
+      msg.override = 0;
+      msg.header.stamp = ros::Time::now();
+
+      throttlePub.publish(msg);
+      if(msg.pedal_input > msg.THROTTLE_MAX) {
+        ROS_WARN("Throttle greater than expected: %d%%", msg.pedal_input);
+      }
+      return;
+  };
+}
+
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+
   ros::init(argc, argv, "mdas_dbw");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
+  ros::NodeHandle n, nh_param("~");
 
-  uint32_t throttle_id, steer_id, brake_id;
-  n.param<std::uint32_t>("throttle_can_id", throttle_id, 0x1ADB0000)
+  std::string throttle_id_str, brake_id_str, steer_id_str, feedback_id_str;
+
+
+  nh_param.param<std::string>("throttle_can_id", throttle_id_str, "0x1ADB0000");
+  nh_param.param<std::string>("brake_can_id", brake_id_str, "0x18DB0000");
+  nh_param.param<std::string>("steering_can_id", steer_id_str, "0x19DB0000");
+  nh_param.param<std::string>("feedback_can_id", feedback_id_str, "0x1CDBFFFF");
+
+  // Convert hex strings to ints
+  throttle_id = std::stoi(throttle_id_str, nullptr, 16);
+  brake_id = std::stoi(brake_id_str, nullptr, 16);
+  steer_id = std::stoi(steer_id_str, nullptr, 16);
+  feedback_id = std::stoi(feedback_id_str, nullptr, 16);
+
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  throttlePub = n.advertise<mdas_dbw_msgs::ThrottleReport>("mdas_dbw/throttle_feedback", 2);
 
-  ros::Rate loop_rate(10);
+  ros::Subscriber feedbackSub = n.subscribe("received_messages", 100, receivedMessagesCallback);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
-  while (ros::ok())
-  {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    can_msgs::Frame canMsg;
-
-    canMsg.id = 0x123;
-
-    ROS_INFO("%s", msg.data.c_str());
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-    chatter_pub.publish(msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    ++count;
-  }
-
+  ros::spin();
 
   return 0;
 }
