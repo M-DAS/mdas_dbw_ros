34ad271adf71dcf21ea97bcddc80952069771f2a
Brandon Smyth <bjsmyth@umich.edu>
2019-08-07 22:19:02 -0400

    Reformat code

--- mdas_dbw_can/src/MdasDbwNode.cpp
+++ mdas_dbw_can/src/MdasDbwNode.cpp
@@ -15,7 +15,7 @@ namespace mdas_dbw_can
         throttle_id = std::stoi(throttle_id_str, nullptr, 16);
         brake_id = std::stoi(brake_id_str, nullptr, 16);
         steer_id = std::stoi(steer_id_str, nullptr, 16);
-        feedback_id = std::stoi(feedback_id_str, nullptr, 16);
+        throttlebrake_feedback_id = std::stoi(feedback_id_str, nullptr, 16);
 
         throttlePub = node.advertise<mdas_dbw_msgs::ThrottleReport>("mdas_dbw/feedback/throttle", 10);
         brakePub = node.advertise<mdas_dbw_msgs::BrakeReport>("mdas_dbw/feedback/brake", 10);
@@ -23,25 +23,25 @@ namespace mdas_dbw_can
         canSender = node.advertise<can_msgs::Frame>("sent_messages", 30);
 
         const ros::TransportHints NODELAY = ros::TransportHints().tcpNoDelay();
-        feedbackSub = node.subscribe("received_messages", 100, &DbwNode::receivedMessagesCallback, this, NODELAY);
-        brakeSub = node.subscribe("mdas_dbw/cmd/brake", 1, &DbwNode::receivedBrakeCmdCallback, this, NODELAY);
-        throttleSub = node.subscribe("mdas_dbw/cmd/throttle", 1, &DbwNode::receivedThrottleCmdCallback, this, NODELAY);
-        steeringSub = node.subscribe("mdas_dbw/cmd/steering", 1, &DbwNode::receivedSteerCmdCallback, this, NODELAY);
+        feedbackSub = node.subscribe("received_messages", 100, &DbwNode::ReceivedMessagesCallback, this, NODELAY);
+        brakeSub = node.subscribe("mdas_dbw/cmd/brake", 1, &DbwNode::ReceivedBrakeCmdCallback, this, NODELAY);
+        throttleSub = node.subscribe("mdas_dbw/cmd/throttle", 1, &DbwNode::ReceivedThrottleCmdCallback, this, NODELAY);
+        steeringSub = node.subscribe("mdas_dbw/cmd/steering", 1, &DbwNode::ReceivedSteerCmdCallback, this, NODELAY);
 
 
-        timer = node.createTimer(ros::Duration(0.1), &DbwNode::canSend, this);
+        timer = node.createTimer(ros::Duration(0.1), &DbwNode::CanSend, this);
     }
 
     DbwNode::~DbwNode() {
     }
 
-    inline can_msgs::Frame DbwNode::buildBrakeMsg(std::uint8_t pedal_cmd) {
+    inline can_msgs::Frame DbwNode::BuildBrakeMsg(std::uint8_t pedal_cmd) {
         can_msgs::Frame msg;
 
         msg.is_extended = true;
         msg.is_error = false;
         msg.is_rtr = false;
-        msg.data[0] = SRC_ID;
+        msg.data[0] = CONTROL_SRC_ID;
         msg.data[1] = 0;
         msg.data[2] = 0;
         msg.dlc = 4;
@@ -53,13 +53,13 @@ namespace mdas_dbw_can
         return msg;
     }
 
-    inline can_msgs::Frame DbwNode::buildThrottleMsg(std::uint8_t pedal_cmd) {
+    inline can_msgs::Frame DbwNode::BuildThrottleMsg(std::uint8_t pedal_cmd) {
         can_msgs::Frame msg;
 
         msg.is_extended = true;
         msg.is_error = false;
         msg.is_rtr = false;
-        msg.data[0] = SRC_ID;
+        msg.data[0] = CONTROL_SRC_ID;
         msg.data[1] = 0;
         msg.data[2] = 0;
         msg.dlc = 4;
@@ -72,7 +72,7 @@ namespace mdas_dbw_can
         return msg;
     }
 
-    inline can_msgs::Frame DbwNode::buildSteeringMsg(std::float_t wheel_cmd) {
+    inline can_msgs::Frame DbwNode::BuildSteeringMsg(std::float_t wheel_cmd) {
         can_msgs::Frame msg;
 
         msg.is_extended = true;
@@ -85,9 +85,11 @@ namespace mdas_dbw_can
 
         msg.id = steer_id;
 
+        // Translate steering angle to command
         float steer = STEER_MULT * wheel_cmd + STEER_ADD;
         std::uint16_t steerInt = (uint16_t)steer;
 
+        // Build command message
         msg.data[2] = (steerInt >> 8) & 0xff;
         msg.data[3] = steerInt & 0xff;
 
@@ -96,38 +98,116 @@ namespace mdas_dbw_can
         return msg;
     }
 
-    void DbwNode::receivedMessagesCallback(const can_msgs::Frame::ConstPtr& canFrame) {
-        if(canFrame->id != feedback_id) {
+    inline mdas_dbw_msgs::BrakeReport DbwNode::BuildBrakeReport(const can_msgs::Frame::ConstPtr& canFrame) {
+        mdas_dbw_msgs::BrakeReport msg;
+
+        msg.enabled = brakeCmd.enable;
+        msg.pedal_input = canFrame->data[2];
+        msg.override = 0;
+        msg.header.stamp = ros::Time::now();
+
+        if(msg.pedal_input > msg.BRAKE_MAX) {
+            ROS_WARN("Brake input greater than expected: %d%%", msg.pedal_input);
+        }
+
+        return msg;
+    }
+
+    inline mdas_dbw_msgs::ThrottleReport DbwNode::BuildThrottleReport(const can_msgs::Frame::ConstPtr& canFrame) {
+        mdas_dbw_msgs::ThrottleReport msg;
+
+        msg.enabled = throttleCmd.enable;
+        msg.pedal_input = canFrame->data[2];
+        msg.override = 0;
+        msg.header.stamp = ros::Time::now();
+
+        if(msg.pedal_input > msg.THROTTLE_MAX) {
+            ROS_WARN("Brake input greater than expected: %d%%", msg.pedal_input);
+        }
+
+        return msg;
+    }
+
+    inline mdas_dbw_msgs::SteeringReport DbwNode::BuildSteeringReport(const can_msgs::Frame::ConstPtr& canFrame) {
+        mdas_dbw_msgs::SteeringReport msg;
+
+        msg.enabled = steerCmd.enable;
+
+        msg.steering_wheel_angle = 0; // TODO: Unused, talk to Naso to resolve this
+        msg.steering_wheel_angle_velocity = 0; // TODO: Unused, talk to Naso to resolve this
+        msg.steering_wheel_torque = 0; // TODO: Unused, talk to Naso to resolve this
+        msg.override = 0;
+
+        msg.header.stamp = ros::Time::now();
+
+        return msg;
+    }
+
+    void DbwNode::TimeoutDisable() {
+        throttleTimeout++;
+        brakeTimeout++;
+        steerTimeout++;
+
+        if(throttleTimeout == TIMEOUT_INTERVAL) {
+            throttleCmd.enable = false;
+            ROS_WARN("Throttle command timeout, disabling");
+        }
+        if(brakeTimeout == TIMEOUT_INTERVAL) {
+            brakeCmd.enable = false;
+            ROS_WARN("Brake command timeout, disabling");
+        }
+        if(steerTimeout == TIMEOUT_INTERVAL ) {
+            steerCmd.enable = false;
+            ROS_WARN("Steering command timeout, disabling");
+        }
+    }
+
+    void DbwNode::ReceivedMessagesCallback(const can_msgs::Frame::ConstPtr& canFrame) {
+        if((canFrame->id != throttlebrake_feedback_id) || (canFrame->id != STEERING_POSITION_CAN_ID) ||
+           (canFrame->id != STEERING_VELOCITY_CAN_ID) || (canFrame->id != STEERING_TORQUE_CAN_ID)) {
             return;
         }
         std::uint8_t srcId = canFrame->data[0];
 
         switch(srcId) {
-            case 1:
-            mdas_dbw_msgs::ThrottleReport msg;
-            msg.enabled = 0;
-            msg.pedal_input = canFrame->data[2];
-            msg.override = 0;
-            msg.header.stamp = ros::Time::now();
-
-            throttlePub.publish(msg);
-            if(msg.pedal_input > msg.THROTTLE_MAX) {
-                ROS_WARN("Throttle greater than expected: %d%%", msg.pedal_input);
+            case BRAKE_FEEDBACK_SRC_ID:
+            {
+                mdas_dbw_msgs::BrakeReport msg = BuildBrakeReport(canFrame);
+                brakePub.publish(msg);
+                return;
             }
-            return;
+            case THROTTLE_FEEDBACK_SRC_ID:
+            {
+                mdas_dbw_msgs::ThrottleReport msg = BuildThrottleReport(canFrame);
+                throttlePub.publish(msg);
+                return;
+            }
+            case STEERING_FEEDBACK_SRC_ID:
+            {
+                mdas_dbw_msgs::SteeringReport msg = BuildSteeringReport(canFrame);
+                steeringPub.publish(msg);
+                return;
+            }
+            default:
+            {
+                ROS_ERROR("Unknown Feedback Source ID: %x", srcId);
+                return;
+            }
         };
     }
 
-    void DbwNode::receivedBrakeCmdCallback(const mdas_dbw_msgs::BrakeCmd& brakeMsg) {
+    void DbwNode::ReceivedBrakeCmdCallback(const mdas_dbw_msgs::BrakeCmd& brakeMsg) {
         brakeCmd = brakeMsg;
 
         if(brakeCmd.pedal_cmd > brakeCmd.BRAKE_MAX) { // Clamp input
             brakeCmd.pedal_cmd = brakeCmd.BRAKE_MAX;
             ROS_WARN("Brake Command >%d%%, input clamped", brakeCmd.BRAKE_MAX);
         }
+
+        brakeTimeout = 0; // Received message, reset timeout
     }
 
-    void DbwNode::receivedSteerCmdCallback(const mdas_dbw_msgs::SteeringCmd& steerMsg) {
+    void DbwNode::ReceivedSteerCmdCallback(const mdas_dbw_msgs::SteeringCmd& steerMsg) {
         steerCmd = steerMsg;
         const std::int32_t steerNeg = steerCmd.ANGLE_MAX * -1;
 
@@ -138,65 +218,48 @@ namespace mdas_dbw_can
             steerCmd.steering_wheel_angle_cmd = steerNeg;
             ROS_WARN("Steering command <%d degrees, input clamped", steerNeg);
         }
+
+        steerTimeout = 0; // Received emssage, reset timeout
     }
 
-    void DbwNode::receivedThrottleCmdCallback(const mdas_dbw_msgs::ThrottleCmd& throttleMsg) {
+    void DbwNode::ReceivedThrottleCmdCallback(const mdas_dbw_msgs::ThrottleCmd& throttleMsg) {
         throttleCmd = throttleMsg;
 
         if(throttleCmd.pedal_cmd > throttleCmd.THROTTLE_MAX) { // Clamp input
             throttleCmd.pedal_cmd = throttleCmd.THROTTLE_MAX;
             ROS_WARN("Steering command >%d%%, input clamped", throttleCmd.THROTTLE_MAX);
         }
+
+        throttleTimeout = 0; // Received message, reset timeout
     }
 
-    void DbwNode::canSend(const ros::TimerEvent&) {
-        static bool brakeState = false, throttleState = false, steerState = false;
-
+    void DbwNode::CanSend(const ros::TimerEvent&) {
         can_msgs::Frame msg;
-        msg.is_extended = true;
-        msg.is_error = false;
-        msg.is_rtr = false;
-        msg.data[0] = SRC_ID;
-        msg.data[1] = 0;
-        msg.data[2] = 0;
-        msg.dlc = 4;
 
         if(brakeCmd.enable) {
-            msg = buildBrakeMsg(brakeCmd.pedal_cmd);
+            msg = BuildBrakeMsg(brakeCmd.pedal_cmd);
+            canSender.publish(msg);
+        } else {
+            msg = BuildBrakeMsg(0);
             canSender.publish(msg);
         }
 
         if(throttleCmd.enable) {
-            msg = buildThrottleMsg(throttleCmd.pedal_cmd);
+            msg = BuildThrottleMsg(throttleCmd.pedal_cmd);
+            canSender.publish(msg);
+        } else {
+            msg = BuildThrottleMsg(0);
             canSender.publish(msg);
         }
 
         if(steerCmd.enable) {
-            msg = buildSteeringMsg(steerCmd.steering_wheel_angle_cmd);
+            msg = BuildSteeringMsg(steerCmd.steering_wheel_angle_cmd);
+            canSender.publish(msg);
+        } else {
+            msg = BuildSteeringMsg(0);
             canSender.publish(msg);
         }
 
-
-        // Check if any enable states changed to disabled
-        // then stop ECU commands
-        if((brakeState == true) && (brakeCmd.enable == false)) {
-            msg = buildBrakeMsg(0);
-            canSender.publish(msg);
-        }
-
-        if((throttleState == true) && (throttleCmd.enable == false)) {
-            msg = buildThrottleMsg(0);
-            canSender.publish(msg);
-        }
-
-        if((steerState == true) && (steerCmd.enable == false)) {
-            msg = buildSteeringMsg(0);
-            canSender.publish(msg);
-        }
-
-        // Save enable state
-        brakeState = brakeCmd.enable;
-        throttleState = throttleCmd.enable;
-        steerState = steerCmd.enable;
+        TimeoutDisable();
     }
 }
--- mdas_dbw_can/src/MdasDbwNode.h
+++ mdas_dbw_can/src/MdasDbwNode.h
@@ -15,10 +15,6 @@
 
 #include <cstdint>
 
-#define SRC_ID (0x02)
-#define STEER_MULT (3.25f)
-#define STEER_ADD (2048)
-
 namespace mdas_dbw_can
 {
     class DbwNode
@@ -29,20 +25,40 @@ namespace mdas_dbw_can
 
         private:
             // Functions
-            inline can_msgs::Frame buildBrakeMsg(std::uint8_t pedal_cmd);
-            inline can_msgs::Frame buildThrottleMsg(std::uint8_t pedal_cmd);
-            inline can_msgs::Frame buildSteeringMsg(std::float_t wheel_cmd);
-            void receivedMessagesCallback(const can_msgs::Frame::ConstPtr& canFrame);
-            void receivedBrakeCmdCallback(const mdas_dbw_msgs::BrakeCmd& brakeMsg);
-            void receivedSteerCmdCallback(const mdas_dbw_msgs::SteeringCmd& steerMsg);
-            void receivedThrottleCmdCallback(const mdas_dbw_msgs::ThrottleCmd& throttleMsg);
-            void canSend(const ros::TimerEvent&);
+            inline can_msgs::Frame BuildBrakeMsg(std::uint8_t pedal_cmd);
+            inline can_msgs::Frame BuildThrottleMsg(std::uint8_t pedal_cmd);
+            inline can_msgs::Frame BuildSteeringMsg(std::float_t wheel_cmd);
+            inline mdas_dbw_msgs::BrakeReport BuildBrakeReport(const can_msgs::Frame::ConstPtr& canFrame);
+            inline mdas_dbw_msgs::ThrottleReport BuildThrottleReport(const can_msgs::Frame::ConstPtr& canFrame);
+            inline mdas_dbw_msgs::SteeringReport BuildSteeringReport(const can_msgs::Frame::ConstPtr& canFrame);
+            void ReceivedMessagesCallback(const can_msgs::Frame::ConstPtr& canFrame);
+            void ReceivedBrakeCmdCallback(const mdas_dbw_msgs::BrakeCmd& brakeMsg);
+            void ReceivedSteerCmdCallback(const mdas_dbw_msgs::SteeringCmd& steerMsg);
+            void ReceivedThrottleCmdCallback(const mdas_dbw_msgs::ThrottleCmd& throttleMsg);
+            void CanSend(const ros::TimerEvent&);
+            void TimeoutDisable();
+
+            // Constants
+            enum STEER_FEEDBACK_IDS:std::uint32_t {
+                STEERING_POSITION_CAN_ID = 0x18FF0113,
+                STEERING_VELOCITY_CAN_ID = 0x18FF0213,
+                STEERING_TORQUE_CAN_ID = 0x18FF0313
+            };
+            enum SRC_ID:std::uint8_t {
+                BRAKE_FEEDBACK_SRC_ID = 0x00,
+                THROTTLE_FEEDBACK_SRC_ID = 0x01,
+                STEERING_FEEDBACK_SRC_ID = 0x02,
+                CONTROL_SRC_ID = 0x02,
+            };
+            static constexpr std::int32_t TIMEOUT_INTERVAL = 5; // 500 ms command timeout
+            static constexpr float STEER_MULT = 3.25f;
+            static constexpr float STEER_ADD = 2048.0f;
 
             // Variables
             std::int32_t throttle_id; 
             std::int32_t steer_id;
             std::int32_t brake_id;
-            std::int32_t feedback_id;
+            std::int32_t throttlebrake_feedback_id;
             ros::Publisher throttlePub;
             ros::Publisher steeringPub;
             ros::Publisher brakePub;
@@ -50,6 +66,9 @@ namespace mdas_dbw_can
             mdas_dbw_msgs::BrakeCmd brakeCmd;
             mdas_dbw_msgs::SteeringCmd steerCmd;
             mdas_dbw_msgs::ThrottleCmd throttleCmd;
+            std::int32_t throttleTimeout = TIMEOUT_INTERVAL + 1;
+            std::int32_t brakeTimeout = TIMEOUT_INTERVAL + 1;
+            std::int32_t steerTimeout = TIMEOUT_INTERVAL + 1;
 
             // Callback variables
             ros::Subscriber feedbackSub;
